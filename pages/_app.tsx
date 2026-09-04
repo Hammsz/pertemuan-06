b787827b6d67e5859e1538c2624aebf25e8e49a7
Hammsz <mhmmd07ilham@gmail.com>
2026-09-04 22:20:35 +0700

    Praktikum 1: Instalasi Bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import("bootstrap");
+  }, []);
+
   return <Component {...pageProps} />;
 }
