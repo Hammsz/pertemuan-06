a463b95918a3372d256d9d5d0f7d762100f4c27d
Hammsz <mhmmd07ilham@gmail.com>
2026-09-04 22:50:35 +0700

    Praktikum 2: Login dengan Redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "../redux/store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     import("bootstrap");
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
---
+++ pages/login.tsx
@@ -0,0 +1,63 @@
+import { useDispatch, useSelector } from "react-redux";
+import { setLogin } from "../redux/auth/authSlice";
+import parse from "html-react-parser";
+
+export default function LoginCheck() {
+  const { isLogin } = useSelector(
+    (state: { auth: { isLogin: boolean } }) => state.auth
+  );
+
+  const dispatch = useDispatch();
+
+  function handleAuth(type = "logout") {
+    if (type === "logout" || type === "login") {
+      dispatch(
+        setLogin({
+          isLogin: type === "login" ? true : false,
+        })
+      );
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="card mt-3">
+            <div className="card-header">Status Login</div>
+
+            <div className="card-body">
+              {isLogin ? (
+                <>
+                  {parse(
+                    '<div class="alert alert-success">Yay, berhasil login!!!</div>'
+                  )}
+
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleAuth("logout")}
+                  >
+                    Log out
+                  </button>
+                </>
+              ) : (
+                <>
+                  {parse(
+                    '<div class="alert alert-dark">Anda telah logout!</div>'
+                  )}
+
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleAuth("login")}
+                  >
+                    Log in
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
